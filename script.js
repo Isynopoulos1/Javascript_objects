f16d6bb350ff9930ed14e452fa8a66b89f89003f
Isela Alarcón <isela.v.alarcon@gmail.com>
2022-11-07 15:49:11 +0100

    random module and trunc module

--- script.js
+++ script.js
@@ -518,3 +518,30 @@ for (let exercise = 1; exercise < 4; exercise++) {
     }
         
 }
+
+//FOR LOOP
+for(let rep = 1; rep <= 10; rep++){
+    console.log(`----STARTING FOR EXERCISE--- ${rep}`)
+}
+
+// WHILE LOOP
+let rep = 1;
+while (rep <= 10){
+    console.log(`----STARTING WHILE EXERCISE--- ${rep}`)
+    rep++;
+}
+
+
+///////////////////////////////////////
+// RANDOM MODULE AND TRUNC MODULE
+// WE USED TRUNC TO ROUND THE NUMBER 
+
+let dice = Math.trunc(Math.random() * 6 )+ 1;
+console.log(dice)
+
+while( dice !== 6){
+    console.log(`you rolled a ${dice}`)
+    dice = Math.trunc(Math.random() * 6 )+ 1;
+    if(dice === 6) console.log('You rolled the magic number ')
+   
+}
