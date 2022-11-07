3f8b6046dda67d97d893c20541be87e61dda9640
Isela Alarcón <isela.v.alarcon@gmail.com>
2022-11-07 17:24:58 +0100

    Loop for arrays

--- script.js
+++ script.js
@@ -248,7 +248,7 @@ GOOD LUCK 😀
 //     }
   
 //   }
-  
+// console.log(calcTip(430))
   ///////////////////////////////////////
   // OPTION 2: TERNARY OPERATOR
   
@@ -495,53 +495,119 @@ GOOD LUCK 😀
 //       console.log(isela[i], typeof isela[i])
 //     }
 
-// ARRAY IN REVERSE 5,4,3,2,1,0
-const isela = [
-    'Isela',
-    'Alarcón',
-    3000 -1988,
-    'Developer',
-    ['Erwie', 'Doudou', 'Gala', 'Romerita'],
-    true
-  ];
+// // ARRAY IN REVERSE 5,4,3,2,1,0
+// const isela = [
+//     'Isela',
+//     'Alarcón',
+//     3000 -1988,
+//     'Developer',
+//     ['Erwie', 'Doudou', 'Gala', 'Romerita'],
+//     true
+//   ];
 
-  for (let i = isela.length -1; i >= 0; i--) {
-    console.log(i, isela[i])
+//   for (let i = isela.length -1; i >= 0; i--) {
+//     console.log(i, isela[i])
    
-  }
+//   }
 
-  // LOOPS INSIDE A LOOP
-for (let exercise = 1; exercise < 4; exercise++) {
-    console.log(`----STARTING EXERCISE--- ${exercise}`)
-    for(let rep = 1; rep < 6; rep++){
-        console.log(`Exercise ${exercise} repetitions 🏋🏼‍♀️${rep}`)
-    }
+//   // LOOPS INSIDE A LOOP
+// for (let exercise = 1; exercise < 4; exercise++) {
+//     console.log(`----STARTING EXERCISE--- ${exercise}`)
+//     for(let rep = 1; rep < 6; rep++){
+//         console.log(`Exercise ${exercise} repetitions 🏋🏼‍♀️${rep}`)
+//     }
         
-}
+// }
 
-//FOR LOOP
-for(let rep = 1; rep <= 10; rep++){
-    console.log(`----STARTING FOR EXERCISE--- ${rep}`)
-}
+// //FOR LOOP
+// for(let rep = 1; rep <= 10; rep++){
+//     console.log(`----STARTING FOR EXERCISE--- ${rep}`)
+// }
 
-// WHILE LOOP
-let rep = 1;
-while (rep <= 10){
-    console.log(`----STARTING WHILE EXERCISE--- ${rep}`)
-    rep++;
-}
+// // WHILE LOOP
+// let rep = 1;
+// while (rep <= 10){
+//     console.log(`----STARTING WHILE EXERCISE--- ${rep}`)
+//     rep++;
+// }
 
 
-///////////////////////////////////////
-// RANDOM MODULE AND TRUNC MODULE
-// WE USED TRUNC TO ROUND THE NUMBER 
+// ///////////////////////////////////////
+// // RANDOM MODULE AND TRUNC MODULE
+// // WE USED TRUNC TO ROUND THE NUMBER 
 
-let dice = Math.trunc(Math.random() * 6 )+ 1;
-console.log(dice)
+// let dice = Math.trunc(Math.random() * 6 )+ 1;
+// console.log(dice)
 
-while( dice !== 6){
-    console.log(`you rolled a ${dice}`)
-    dice = Math.trunc(Math.random() * 6 )+ 1;
-    if(dice === 6) console.log('You rolled the magic number ')
+// while( dice !== 6){
+//     console.log(`you rolled a ${dice}`)
+//     dice = Math.trunc(Math.random() * 6 )+ 1;
+//     if(dice === 6) console.log('You rolled the magic number ')
    
+// }
+///////////////////////////////////////
+// Coding Challenge #4
+
+/*
+Let's improve Steven's tip calculator even more, this time using loops!
+
+1. Create an array 'bills' containing all 10 test bill values
+2. Create empty arrays for the tips and the totals ('tips' and 'totals')
+3. Use the 'calcTip' function we wrote before (no need to repeat) to calculate tips and total values (bill + tip) for every bill value in the bills array. Use a for loop to perform the 10 calculations!
+
+TEST DATA: 22, 295, 176, 440, 37, 105, 10, 1100, 86 and 52
+
+HINT: Call calcTip in the loop and use the push method to add values to the tips and totals arrays 😉
+
+4. BONUS: Write a function 'calcAverage' which takes an array called 'arr' as an argument. This function calculates the average of all numbers in the given array. This is a DIFFICULT challenge (we haven't done this before)! Here is how to solve it:
+  4.1. First, you will need to add up all values in the array. To do the addition, start by creating a variable 'sum' that starts at 0. Then loop over the array using a for loop. In each iteration, add the current value to the 'sum' variable. This way, by the end of the loop, you have all values added together
+  4.2. To calculate the average, divide the sum you calculated before by the length of the array (because that's the number of elements)
+  4.3. Call the function with the 'totals' array
+
+GOOD LUCK 😀
+*/
+
+
+//CALC TIP FUNCTION
+const calcTip2= (bill) => {
+  return bill <= 300 && bill >= 50 
+  ? bill * 0.15 
+  : bill * 0.2;
 }
+// console.log(calcTip2(22))
+
+
+
+
+
+//CREATING BILLS ARRAY AND EMPTY ARRAYS OF TIPS AND TOTALS
+const bills =[22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+const tips=[];
+const totals =[];
+
+// CREATING A FOR LOOP TO GET THE BILLS FOR EACH VALUE IN THE BILLS ARRAY 
+
+for (let i = 0; i < bills.length; i++) {
+  const tip = calcTip2 (bills[i])
+
+  //ADDING THE METHOD PUSH IN THE TIPS ARRAY VALUES
+  tips.push(tip);
+  
+   //ADDING THE METHOD PUSH TO SUM TIPS + BILLS 
+  totals.push(tip + bills[i]);
+}
+// PRINTINS THE FINAL VALUES 
+ console.log(bills, tips, totals)
+
+
+
+
+
+
+
+
+
+
+
+
+   
