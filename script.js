e23c3b9575573308c429ba0ece2adf4dfd03eb12
Isela Alarcón <isela.v.alarcon@gmail.com>
2022-11-15 17:31:31 +0100

    concateneting 2 arrays

--- script.js
+++ script.js
@@ -590,3 +590,33 @@ console.log(amplitude);
 
 // PROBLEM 2:
 // Function should now receive 2 arrays of temps
+
+const calcTempAmplitudeNew = function (t1, t2) {
+  /*
+EXAMPLE:
+
+const array1= ['a', 'b', 'c'];
+const array2= ['d', 'e', 'f'];
+const array3 = array1.concat(array2);
+
+console .log(array3);
+expected output: Array ["a", "b", "c", "d", "e", "f"] 
+*/
+  const temps = t1.concat(t2);
+  console.log(temps);
+  let max = temps[0];
+  let min = temps[0];
+
+  for (let i = 0; i < temps.length; i++) {
+    const curTemp = temps[i];
+    if (typeof curTemp !== "number") continue;
+
+    if (curTemp > max) max = curTemp;
+    if (curTemp < min) min = curTemp;
+  }
+  console.log(max, min);
+  return max - min;
+};
+
+const amplitudeNew = calcTempAmplitudeNew([3, 5, 1], [9, 0, 3]);
+console.log(amplitudeNew);
