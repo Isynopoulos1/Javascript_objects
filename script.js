58a1cb52728486d411b838dfc427940aaca9cf33
Isela Alarcón <isela.v.alarcon@gmail.com>
2022-11-15 17:10:17 +0100

    calculating the amplitude using an array

--- script.js
+++ script.js
@@ -561,16 +561,32 @@ GOOD LUCK 😀
 // const result = calcAverage(totals);
 // console.log(result);
 
+///////////////////////////////////////
+// Using Google, StackOverflow and MDN
+
+// PROBLEM 1:
+// We work for a company building a smart home thermometer. Our most recent task is this: "Given an array of temperatures of one day, calculate the temperature amplitude. Keep in mind that sometimes there might be a sensor error."
+
 //// CALCULATING TEMPERATURE IN ARRAY
+const temperatures = [3, -2, -6, -1, "error", 9, 13, 17, 15, 14, 9, 5];
 
 const calcTempAmplitude = function (temps) {
   let max = temps[0];
   let min = temps[0];
+
   for (let i = 0; i < temps.length; i++) {
     const curTemp = temps[i];
+    if (typeof curTemp !== "number") continue;
+
     if (curTemp > max) max = curTemp;
     if (curTemp < min) min = curTemp;
   }
   console.log(max, min);
+  return max - min;
 };
-calcTempAmplitude([3, 7, 4, 35, 1, -2]);
+
+const amplitude = calcTempAmplitude(temperatures);
+console.log(amplitude);
+
+// PROBLEM 2:
+// Function should now receive 2 arrays of temps
