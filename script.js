14c37600a4b090f469041e8ed45c39aede62ca34
Isela Alarcón <isela.v.alarcon@gmail.com>
2022-11-16 19:41:15 +0100

    Debbuging with the console and breakpoints

--- script.js
+++ script.js
@@ -568,55 +568,76 @@ GOOD LUCK 😀
 // We work for a company building a smart home thermometer. Our most recent task is this: "Given an array of temperatures of one day, calculate the temperature amplitude. Keep in mind that sometimes there might be a sensor error."
 
 //// CALCULATING TEMPERATURE IN ARRAY
-const temperatures = [3, -2, -6, -1, "error", 9, 13, 17, 15, 14, 9, 5];
+// const temperatures = [3, -2, -6, -1, "error", 9, 13, 17, 15, 14, 9, 5];
 
-const calcTempAmplitude = function (temps) {
-  let max = temps[0];
-  let min = temps[0];
+// const calcTempAmplitude = function (temps) {
+//   let max = temps[0];
+//   let min = temps[0];
 
-  for (let i = 0; i < temps.length; i++) {
-    const curTemp = temps[i];
-    if (typeof curTemp !== "number") continue;
+//   for (let i = 0; i < temps.length; i++) {
+//     const curTemp = temps[i];
+//     if (typeof curTemp !== "number") continue;
 
-    if (curTemp > max) max = curTemp;
-    if (curTemp < min) min = curTemp;
-  }
-  console.log(max, min);
-  return max - min;
+//     if (curTemp > max) max = curTemp;
+//     if (curTemp < min) min = curTemp;
+//   }
+//   console.log(max, min);
+//   return max - min;
+// };
+
+// const amplitude = calcTempAmplitude(temperatures);
+// console.log(amplitude);
+
+// // PROBLEM 2:
+// // Function should now receive 2 arrays of temps
+
+// const calcTempAmplitudeNew = function (t1, t2) {
+//   /*
+// EXAMPLE:
+
+// const array1= ['a', 'b', 'c'];
+// const array2= ['d', 'e', 'f'];
+// const array3 = array1.concat(array2);
+
+// console .log(array3);
+// expected output: Array ["a", "b", "c", "d", "e", "f"]
+// */
+//   const temps = t1.concat(t2);
+//   console.log(temps);
+//   let max = temps[0];
+//   let min = temps[0];
+
+//   for (let i = 0; i < temps.length; i++) {
+//     const curTemp = temps[i];
+//     if (typeof curTemp !== "number") continue;
+
+//     if (curTemp > max) max = curTemp;
+//     if (curTemp < min) min = curTemp;
+//   }
+//   console.log(max, min);
+//   return max - min;
+// };
+
+// const amplitudeNew = calcTempAmplitudeNew([3, 5, 1], [9, 0, 3]);
+// console.log(amplitudeNew);
+
+// //////////////////////////DEBUGGING
+const MeasureKelvin = function () {
+  const measurement = {
+    type: "temp",
+    unit: "celsius",
+
+    //C) FIX
+    value: Number(prompt("Degrees Celsius: ")),
+  };
+
+  //B FIND
+  console.table(measurement);
+  console.log(measurement.value);
+
+  const kelvin = measurement.value + 273;
+  return kelvin;
 };
 
-const amplitude = calcTempAmplitude(temperatures);
-console.log(amplitude);
-
-// PROBLEM 2:
-// Function should now receive 2 arrays of temps
-
-const calcTempAmplitudeNew = function (t1, t2) {
-  /*
-EXAMPLE:
-
-const array1= ['a', 'b', 'c'];
-const array2= ['d', 'e', 'f'];
-const array3 = array1.concat(array2);
-
-console .log(array3);
-expected output: Array ["a", "b", "c", "d", "e", "f"] 
-*/
-  const temps = t1.concat(t2);
-  console.log(temps);
-  let max = temps[0];
-  let min = temps[0];
-
-  for (let i = 0; i < temps.length; i++) {
-    const curTemp = temps[i];
-    if (typeof curTemp !== "number") continue;
-
-    if (curTemp > max) max = curTemp;
-    if (curTemp < min) min = curTemp;
-  }
-  console.log(max, min);
-  return max - min;
-};
-
-const amplitudeNew = calcTempAmplitudeNew([3, 5, 1], [9, 0, 3]);
-console.log(amplitudeNew);
+// A) IDENTIFY
+console.log(MeasureKelvin());
